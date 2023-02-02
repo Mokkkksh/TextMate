3981acc2734c9ea0ca478b4d177f716ab29efcd5
Mokkkksh <mokkkksh@icloud.com>
2023-02-02 18:52:18 +0530

    Upload

---
+++ backEnd.py
@@ -0,0 +1,89 @@
+import easyocr
+import pyttsx3
+from gtts import gTTS
+from pygame import mixer
+import pandas as pd
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+from matplotlib import font_manager
+import tensorflow as tf
+from tensorflow import keras
+
+
+#Initialize the file path variable
+inputPath = ""
+outputPath = ""
+inputPathEntered = False
+
+outputText = ""
+languageCodeDictionary = {"English": "en", "Hindi": "hi"}
+fontDictionary = {"English": "fonts/aovel.ttf", "Hindi": "fonts/mangal.TTF"}
+
+def processImage(languageCode):
+    imgReader = easyocr.Reader([languageCode])
+    tempText = imgReader.readtext(inputPath, paragraph=True)
+    global outputText
+    outputText = ""
+    for i in tempText:
+        outputText = outputText + i[1] + " "
+    print(outputText)
+
+def saveFile():
+    with open(outputPath, 'w') as f:
+        f.write(outputText)
+
+def textToSpeech(languageCode):
+    tts = gTTS(text=outputText, lang=languageCode)
+    tts.save("tts_audio/tts.mp3")
+    mixer.init()
+    mixer.music.load("tts_audio/tts.mp3")
+    mixer.music.play()
+
+def createDataFrame():
+    # create a list of words from outputText
+    outputTextList = outputText.split()
+    # create a dictionary of words and their frequency
+    wordFrequency = {}
+    for word in outputTextList:
+        if word in wordFrequency:
+            wordFrequency[word] += 1
+        else:
+            wordFrequency[word] = 1
+    # create a dataframe of words and their frequency
+    wordFrequencyDataFrame = pd.DataFrame(list(wordFrequency.items()), columns=['Word', 'Frequency'])
+    # sort the dataframe by frequency
+    wordFrequencyDataFrame = wordFrequencyDataFrame.sort_values(by=['Frequency'], ascending=False)
+    return (wordFrequency, wordFrequencyDataFrame)
+
+def analyseText(language):
+    fontsplt = font_manager.findSystemFonts(fontpaths='/Users/moksh/Documents/Development/TextMateGUI/fonts')
+    for font in fontsplt:
+        font_manager.fontManager.addfont(font)
+    if language == 'English':
+        plt.rcParams['font.family'] = 'aovel'
+    elif language == 'Hindi':
+        plt.rcParams['font.family'] = 'mangal'
+    createDataFrame()[1].plot(x='Word', y='Frequency', kind='bar', figsize=(15, 10), legend=False, grid=True, fontsize=12)
+    plt.title('Word Frequency', fontsize=15, font='aovel')
+    plt.xlabel('Word', fontsize=15, font='aovel')
+    plt.ylabel('Frequency', fontsize=15, font='aovel')
+    plt.show()
+    
+def generateWordCloud(language):
+     # create a word cloud
+    wordcloud = WordCloud(font_path=fontDictionary[language]).generate_from_frequencies(createDataFrame()[0])
+    plt.figure(figsize=(15, 10))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.show()
+
+def loadModel():
+    json_file = open('ocr_training/saved_models/cnnModel.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = tf.keras.models.model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights("ocr_training/saved_models/cnnModel.h5")
+    print("Loaded model from disk")
+    return loaded_model
+
